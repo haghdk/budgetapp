33e2c3f2e02e99c20d1fd98d1cbed61716e2648b
Morten Jensen <morten@hagh.dk>
2024-07-11 21:45:40 +0200

    Get body

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 app.use(express.urlencoded({ extended: true }))
-
+app.use(express.json())
 app.use(`/api/${VERSION}/budget`, budgetRoutes)
 app.use(`/api/${VERSION}/spending`, spendingRoutes)
 app.use(`/api/${VERSION}/category`, categoryRoutes)
