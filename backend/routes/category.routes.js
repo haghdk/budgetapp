524a0e81f937830340b6866f5c5384e8f65a991a
Morten Jensen <morten@hagh.dk>
2024-07-12 20:09:32 +0200

    Route to add new category

--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const categoryController = require('../controllers/category.controller')
 
 router.get('/all', (req, res) => categoryController.listAllCategories(req, res))
+router.post('/add', (req, res) => categoryController.addCategory(req, res))
 
 module.exports = router
