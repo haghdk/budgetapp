145d8db96653f8b4ce3099160864d0682bbcd808
Morten Jensen <morten@hagh.dk>
2024-07-13 18:43:33 +0200

    Add delete endpoint

--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -4,5 +4,6 @@ const categoryController = require('../controllers/category.controller')
 
 router.get('/all', (req, res) => categoryController.listAllCategories(req, res))
 router.post('/add', (req, res) => categoryController.addCategory(req, res))
+router.delete('/delete/:categoryId', (req, res) => categoryController.deleteCategory(req, res))
 
 module.exports = router
