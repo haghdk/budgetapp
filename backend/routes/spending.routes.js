de1b9bc9c336f14cdc20eff405c92efc523e244b
Morten Jensen <morten@hagh.dk>
2024-07-13 18:43:58 +0200

    Remove endpoint for total amount as this has been placed in the listall method

--- backend/routes/spending.routes.js
+++ backend/routes/spending.routes.js
@@ -6,6 +6,5 @@ router.post('/add', (req, res) => spendingController.addSpending(req, res))
 router.get('/list/:budgetId', (req, res) => spendingController.listSpendingsInBudget(req, res))
 router.get('/spendingitem/:spendingId', (req, res) => spendingController.getSpendingById(req, res))
 router.put('/edit', (req, res) => spendingController.editSpending(req, res))
-router.get('/total/:budgetId', (req, res) => spendingController.totalSpending(req, res))
 
 module.exports = router
