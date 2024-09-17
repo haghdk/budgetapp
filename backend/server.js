3b808790588721cf70e98673907df70d14fce33a
Morten Jensen <morten@hagh.dk>
2024-09-17 20:02:44 +0200

    Add auth routes

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,19 @@ const budgetRoutes = require("./routes/budget.routes");
 const spendingRoutes = require("./routes/spending.routes");
 const categoryRoutes = require("./routes/category.routes");
 const incomeRoutes = require("./routes/income.routes");
+const authRoutes = require("./routes/auth.routes");
 const VERSION = "v1";
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.set('trust proxy', false);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(`/api/${VERSION}/budget`, budgetRoutes);
 app.use(`/api/${VERSION}/spending`, spendingRoutes);
 app.use(`/api/${VERSION}/category`, categoryRoutes);
 app.use(`/api/${VERSION}/income`, incomeRoutes);
+app.use(`/api/${VERSION}/auth`, authRoutes);
 
 app.use((err, req, res, next) => {
     if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
